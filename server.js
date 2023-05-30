91b2b32190b2e0ace3c74038abca96ddd38fc5df
screen-t <yassinemt08@gmail.com>
2023-05-30 22:53:12 +0100

    update adding new user status

--- server.js
+++ server.js
@@ -6,23 +6,27 @@ const User= require("./models/User")
 require("./config/connect")
 app.use(express.json())
 
-app.post("/signIn",(request , response)=>{
-    console.log("adding user")
-    data = request.body ;
-    user = new User(data) ;
-    user.save().then(
-        (userInfo)=>{
-            response.send(userInfo)
+app.post("/signIn", async (request , response)=>{
+    try{
+        console.log("adding user")
+        data = request.body ;
+        var user = new User(data) ;
+        var userInfo= await user.save() 
+        var status  = {
+            "message" : "user added successfully",
+            "userInfo": userInfo
+        } 
+        response.send(status)
+    }
+    catch (error) {
+        var status = {
+            "message" : "error in adding the new user",
+            "error": error
         }
-    ).catch(
-        (error)=>{
-            response.send(error)
-        }
-    )
+        response.send(status)
+    }
+
 })
-
-
-
 
 app.listen(3000,()=>{
     console.log("server on")
